cc5c1fac1cbdec9e2db528e09f432cc55ed0a3a6
mrgolu <64455981+mrgolu@users.noreply.github.com>
2025-07-03 09:55:16 +0530

    Fix Capacitor plugins import error

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,10 @@ import { ToastContainer } from 'react-toastify'
 import App from './App'
 import './index.css'
 import 'react-toastify/dist/ReactToastify.css'
-import { initializeCapacitorPlugins } from './capacitor-plugins'
+import { initializeApp } from './capacitor-plugins'
 
 // Initialize Capacitor plugins
-initializeCapacitorPlugins();
+initializeApp();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
